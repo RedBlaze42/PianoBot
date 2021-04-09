0cfbedf257f66213b1e400732281da63d3dea3f0
RedBlaze42 <allairehugo@gmail.com>
2021-04-09 14:18:09 +0200

    Fix

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def on_message(message):
                 await event.remove_participant(user.id)
                 await message.delete()
                 await message.channel.send("{} a été désinscrit de l'évènement {}".format(user.name,event.name),delete_after=10)
-            elif len(args)>0 and args[0]=="write" and message.author.voice is not None:
+            elif len(args)>0 and args[0]=="write":
                 await message.channel.send(" ".join(args[1:]))
                 await message.delete()
             elif len(args)>0 and args[0]=="say" and message.author.voice is not None:
