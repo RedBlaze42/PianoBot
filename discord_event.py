a674cacbe3d6805080d797af5bf05c6a36aa62c8
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 22:03:06 +0200

    Notif quand quelqu'un s'est désisté

--- discord_event.py
+++ discord_event.py
@@ -64,7 +64,11 @@ class DiscordEvent():
                 self.participants.append(id)
         await self.update_message()
 
-    async def remove_participant(self,id):
+    async def remove_participant(self, id, notif=True):
+        if notif and id in self.participants[:self.max_participants]:
+            next_participant = await self.bot.fetch_user(self.participants[self.max_participants])
+            await next_participant.send("Quelqu'un s'est désisté dans les participants de l'évènement {}, tu viens donc de rejoindre la liste principale !\nLien vers le message d'inscription: {}".format(self.name,self.message.jump_url))
+
         if id in self.participants:
             self.participants.remove(id)
         await self.update_message()
