716d3534578234454dd4f8afcf79958e2aa082c9
RedBlaze42 <allairehugo@gmail.com>
2021-04-11 17:30:44 +0200

    Voix

--- commands.py
+++ commands.py
@@ -5,15 +5,20 @@ import discord
 async def say(message,args,bot):
     content=" ".join(args[1:])
     if content!="":
-        tts=gTTS(content,lang="fr")
+        if message.author.id!=70046836743745536:
+            tts=gTTS(content,lang="fr",tld="fr")
+        else:
+            tts=gTTS(content,lang="fr",tld="ca")
         tts.save("temp.mp3")
         if len(bot.voice_clients)>0:
             voice=bot.voice_clients[0]
             if voice.channel.id!=message.author.voice.channel.id:
                 await voice.disconnect()
                 voice = await message.author.voice.channel.connect()
+                await sleep(1000)
         else:
             voice = await message.author.voice.channel.connect()
-        sleep(1000)
+            await sleep(1000)
+        
         source=discord.FFmpegPCMAudio("temp.mp3")
         voice.play(source, after=None)
